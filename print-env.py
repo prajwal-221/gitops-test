a7532176749a4e641b9762e3bf5582c627f39758
prajwal-221 <prajwaln@heaptrace.com>
2025-06-06 15:57:28 +0530

    print-env file

---
+++ print-env.py
@@ -0,0 +1,22 @@
+import os
+
+def print_important_env_vars():
+    important_vars = [
+        "HOME",
+        "USER",
+        "SHELL",
+        "PATH",
+        "LANG",
+        "PWD",
+        "LOGNAME",
+        "TERM",
+        "HOSTNAME",
+        "EDITOR"
+    ]
+
+    for var in important_vars:
+        value = os.getenv(var, "<Not Set>")
+        print(f"{var}={value}")
+
+if __name__ == "__main__":
+    print_important_env_vars()
